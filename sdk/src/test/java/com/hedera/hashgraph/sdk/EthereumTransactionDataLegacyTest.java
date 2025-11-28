b45b67ffce7301fd71b0126763d0ae8a64043fba
anvex21 <alkinvejdi@gmail.com>
2025-11-28 10:35:49 +0200

    Changes in EthereumTransactionDataLegacyTest

--- sdk/src/test/java/com/hedera/hashgraph/sdk/EthereumTransactionDataLegacyTest.java
+++ sdk/src/test/java/com/hedera/hashgraph/sdk/EthereumTransactionDataLegacyTest.java
@@ -20,59 +20,102 @@ public class EthereumTransactionDataLegacyTest {
         "f864012f83018000947e3a9eaf9bcc39e2ffa38eb30bf7a93feacbc18180827653820277a0f9fbff985d374be4a55f296915002eec11ac96f1ce2df183adf992baa9390b2fa00c1e867cc960d9c74ec2e6a662b7908ec4c8cc9f3091e886bcefbeb2290000";
     static final String RAW_TX_TYPE_2 =
         "02f87082012a022f2f83018000947e3a9eaf9bcc39e2ffa38eb30bf7a93feacbc181880de0b6b3a764000083123456c001a0df48f2efd10421811de2bfb125ab75b2d3c44139c4642837fb1fccce911fd479a01aaf7ae92bee896651dfc9d99ae422a296bf5d9f1ca49b2d96d82b79eb112d66";
-    static final byte[] CHAIN_ID = Hex.decode("012a");
-    static final byte[] NONCE = Hex.decode("03");
-    static final byte[] MAX_PRIORITY = Hex.decode("2f");
-    static final byte[] MAX_FEE = Hex.decode("30");
-    static final byte[] GAS_LIMIT = Hex.decode("018000");
-    static final byte[] TO = Hex.decode("7e3a9eaf9bcc39e2ffa38eb30bf7a93feacbc181");
-    static final byte[] VALUE = Hex.decode("0de0b6b3a7640000");
-    static final byte[] CALL_DATA = Hex.decode("12345678");
-    static final byte[] ACCESS_ADDRESS = Hex.decode("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    static final byte[] ACCESS_STORAGE_KEY =
-        Hex.decode("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-    static final byte[] AUTH_CHAIN_ID = Hex.decode("012a");
-    static final byte[] AUTH_ADDRESS = Hex.decode("cccccccccccccccccccccccccccccccccccccccc");
-    static final byte[] AUTH_NONCE = Hex.decode("01");
-    static final byte[] AUTH_Y_PARITY = Hex.decode("01");
-    static final byte[] AUTH_R =
-        Hex.decode("1111111111111111111111111111111111111111111111111111111111111111");
-    static final byte[] AUTH_S =
-        Hex.decode("2222222222222222222222222222222222222222222222222222222222222222");
-    static final byte[] SIG_Y_PARITY = Hex.decode("01");
-    static final byte[] SIG_R =
-        Hex.decode("3333333333333333333333333333333333333333333333333333333333333333");
-    static final byte[] SIG_S =
-        Hex.decode("4444444444444444444444444444444444444444444444444444444444444444");
 
-    static final List<Object> ACCESS_LIST_ENCODING =
-        List.of(List.of(ACCESS_ADDRESS, List.of(ACCESS_STORAGE_KEY)));
-    static final List<Object> AUTH_LIST_ENCODING =
-        List.of(List.of(AUTH_CHAIN_ID, AUTH_ADDRESS, AUTH_NONCE, AUTH_Y_PARITY, AUTH_R, AUTH_S));
+    private record Eip7702Fixture(
+        byte[] chainId,
+        byte[] nonce,
+        byte[] maxPriorityFee,
+        byte[] maxFee,
+        byte[] gasLimit,
+        byte[] to,
+        byte[] value,
+        byte[] callData,
+        List<List<Object>> accessListEncoding,
+        List<List<Object>> authorizationListEncoding,
+        List<EthereumTransactionDataEip7702.AccessListEntry> accessListEntries,
+        byte[] signatureYParity,
+        byte[] signatureR,
+        byte[] signatureS,
+        String rawTx) {
+    }
 
-    static final String RAW_TX_TYPE_4 = Hex.toHexString(RLPEncoder.sequence(
-        Integers.toBytes(0x04),
-        List.of(
-            CHAIN_ID,
-            NONCE,
-            MAX_PRIORITY,
-            MAX_FEE,
-            GAS_LIMIT,
-            TO,
-            VALUE,
-            CALL_DATA,
-            ACCESS_LIST_ENCODING,
-            AUTH_LIST_ENCODING,
-            SIG_Y_PARITY,
-            SIG_R,
-            SIG_S)));
+    private static final Eip7702Fixture EIP_7702_FIXTURE = buildEip7702Fixture();
+
+    static final String RAW_TX_TYPE_4 = EIP_7702_FIXTURE.rawTx();
+
+    private static Eip7702Fixture buildEip7702Fixture() {
+        var chainId = Hex.decode("012a");
+        var nonce = Hex.decode("03");
+        var maxPriority = Hex.decode("2f");
+        var maxFee = Hex.decode("30");
+        var gasLimit = Hex.decode("018000");
+        var to = Hex.decode("7e3a9eaf9bcc39e2ffa38eb30bf7a93feacbc181");
+        var value = Hex.decode("0de0b6b3a7640000");
+        var callData = Hex.decode("12345678");
+        var accessAddress = Hex.decode("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        var accessStorageKey = Hex.decode("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        var authorizationChainId = Hex.decode("012a");
+        var authorizationAddress = Hex.decode("cccccccccccccccccccccccccccccccccccccccc");
+        var authorizationNonce = Hex.decode("01");
+        var authorizationYParity = Hex.decode("01");
+        var authorizationR = Hex.decode("1111111111111111111111111111111111111111111111111111111111111111");
+        var authorizationS = Hex.decode("2222222222222222222222222222222222222222222222222222222222222222");
+        var signatureYParity = Hex.decode("01");
+        var signatureR = Hex.decode("3333333333333333333333333333333333333333333333333333333333333333");
+        var signatureS = Hex.decode("4444444444444444444444444444444444444444444444444444444444444444");
+
+        List<List<Object>> accessListEncoding = List.of(List.of(accessAddress, List.of(accessStorageKey)));
+        List<List<Object>> authorizationListEncoding = List.of(List.of(
+            authorizationChainId,
+            authorizationAddress,
+            authorizationNonce,
+            authorizationYParity,
+            authorizationR,
+            authorizationS));
+
+        var rawTxType4 = Hex.toHexString(RLPEncoder.sequence(
+            Integers.toBytes(0x04),
+            List.of(
+                chainId,
+                nonce,
+                maxPriority,
+                maxFee,
+                gasLimit,
+                to,
+                value,
+                callData,
+                accessListEncoding,
+                authorizationListEncoding,
+                signatureYParity,
+                signatureR,
+                signatureS)));
+
+        var accessListEntries = List.of(new EthereumTransactionDataEip7702.AccessListEntry(
+            accessAddress, List.of(accessStorageKey)));
+
+        return new Eip7702Fixture(
+            chainId,
+            nonce,
+            maxPriority,
+            maxFee,
+            gasLimit,
+            to,
+            value,
+            callData,
+            accessListEncoding,
+            authorizationListEncoding,
+            accessListEntries,
+            signatureYParity,
+            signatureR,
+            signatureS,
+            rawTxType4);
+    }
 
     @Test
     public void legacyToFromBytes() {
         var data = (EthereumTransactionDataLegacy) EthereumTransactionData.fromBytes(Hex.decode(RAW_TX_TYPE_0));
         assertThat(RAW_TX_TYPE_0).isEqualTo(Hex.toHexString(data.toBytes()));
 
-        // Chain ID is not part of the legacy ethereum transaction, so why are you calculating and checking it?
         // assertEquals("012a", Hex.toHexString(data.chainId()));
 
         assertThat(Hex.toHexString(data.nonce)).isEqualTo("01");
@@ -87,7 +130,7 @@ public class EthereumTransactionDataLegacyTest {
         assertThat(Hex.toHexString(data.s))
             .isEqualTo("0c1e867cc960d9c74ec2e6a662b7908ec4c8cc9f3091e886bcefbeb2290fb792");
 
-        // We don't currently support a way to get the ethereum has, but we probably should
+        // currently no way to get ethereum gas
         // assertEquals("9ffbd69c44cf643ed8d1e756b505e545e3b5dd3a6b5ef9da1d8eca6679706594",
         //    Hex.toHexString(data.getEthereumHash()));
     }
@@ -115,6 +158,7 @@ public class EthereumTransactionDataLegacyTest {
 
     @Test
     void eip7702ToFromBytes() {
+        var fixture = EIP_7702_FIXTURE;
         var data = (EthereumTransactionDataEip7702) EthereumTransactionData.fromBytes(Hex.decode(RAW_TX_TYPE_4));
 
         assertThat(RAW_TX_TYPE_4).isEqualTo(Hex.toHexString(data.toBytes()));
@@ -151,24 +195,24 @@ public class EthereumTransactionDataLegacyTest {
 
     @Test
     void throwsOnEmptyAuthorizationList() {
-        var accessList = new ArrayList<EthereumTransactionDataEip7702.AccessListEntry>();
-        accessList.add(new EthereumTransactionDataEip7702.AccessListEntry(
-            ACCESS_ADDRESS, List.of(ACCESS_STORAGE_KEY)));
+        var fixture = EIP_7702_FIXTURE;
+
+        var accessList = new ArrayList<>(fixture.accessListEntries);
 
         var data = new EthereumTransactionDataEip7702(
-            CHAIN_ID,
-            NONCE,
-            MAX_PRIORITY,
-            MAX_FEE,
-            GAS_LIMIT,
-            TO,
-            VALUE,
-            CALL_DATA,
+            fixture.chainId,
+            fixture.nonce,
+            fixture.maxPriorityFee,
+            fixture.maxFee,
+            fixture.gasLimit,
+            fixture.to,
+            fixture.value,
+            fixture.callData,
             accessList,
             List.of(),
-            SIG_Y_PARITY,
-            SIG_R,
-            SIG_S);
+            fixture.signatureYParity,
+            fixture.signatureR,
+            fixture.signatureS);
 
         assertThrows(IllegalStateException.class, data::toBytes);
     }
